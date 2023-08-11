e6982d0af8f9399a08bc9a49024896b3140a1151
chadda <chadha.essid@insat.ucar.tn>
2023-08-11 14:04:07 +0100

    finalized skillpage

--- portfolio-react-app/src/HomePage.jsx
+++ portfolio-react-app/src/HomePage.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
 import  sideDotsPicture from '../public/images/side-dots.png'
 import {useTypewriter,Cursor} from "react-simple-typewriter";
 import ComputerSVG from "./assets/ComputerSVG.jsx";
 import Computer from '../public/images/computer.png'
 import './assets/css/homepage.css'
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import "animate.css/animate.min.css";
 
 export default function HomePage(){
     const [text]=useTypewriter({
         words:['Software Engineering Student.','Beginner Fullstack Developer.','Problem Solver.'],
         loop:{},
     });
+    useEffect(() => {
+        AOS.init({
+            once: false,
+            mirror: true
+        });
+    }, [])
     return(
         <div className="homepage-container">
             <div  className="hero">
@@ -17,7 +26,9 @@ export default function HomePage(){
                     <img className="computer" src={Computer}/>
                     <img className="dots-background" src={sideDotsPicture}/>
                 </div>
-                <div className="description">
+                <div className="description"
+                     data-aos="zoom-in"
+                     data-aos-duration="1500">
                     <div>
                     <h2 className="Iam">My name is Chadha Essid I am a </h2>
                     <div className="words">
